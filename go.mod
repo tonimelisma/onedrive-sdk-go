93e3b1120ad404a041f850ae10c5471bdbb14051
Toni Melisma <59022391+tonimelisma@users.noreply.github.com>
2023-11-27 23:14:03 -0800

    Add error handling

--- go.mod
+++ go.mod
@@ -1,3 +1,15 @@
 module github.com/tonimelisma/onedrive-sdk-go
 
 go 1.19
+
+require (
+	github.com/nirasan/go-oauth-pkce-code-verifier v0.0.0-20220510032225-4f9f17eaec4c
+	golang.org/x/oauth2 v0.15.0
+)
+
+require (
+	github.com/golang/protobuf v1.5.3 // indirect
+	golang.org/x/net v0.19.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
+)
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package onedrive
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
@@ -20,8 +21,13 @@ var (
 )
 
 // apiCall handles the HTTP GET request and categorizes common errors.
-func apiCall(client *http.Client, url string) (*http.Response, error) {
-	res, err := client.Get(url)
+func apiCall(client *http.Client, method, url string) (*http.Response, error) {
+	req, err := http.NewRequest(method, url, nil)
+	if err != nil {
+		return nil, fmt.Errorf("creating request failed: %v", err)
+	}
+
+	res, err := client.Do(req)
 	if err != nil {
 		var oauth2RetrieveError *oauth2.RetrieveError
 		if errors.As(err, &oauth2RetrieveError) {
@@ -59,13 +65,85 @@ func apiCall(client *http.Client, url string) (*http.Response, error) {
 		defer res.Body.Close()
 		resBody, _ := io.ReadAll(res.Body)
 
+		var oneDriveError struct {
+			Error struct {
+				Code    string `json:"code"`
+				Message string `json:"message"`
+			} `json:"error"`
+		}
+		json.Unmarshal(resBody, &oneDriveError)
+
+		// Handling based on HTTP status codes
 		switch res.StatusCode {
-		case http.StatusUnauthorized:
-			return nil, err
-		case http.StatusInternalServerError:
-			return nil, err
+		case http.StatusBadRequest, http.StatusMethodNotAllowed,
+			http.StatusNotAcceptable:
+			return nil, fmt.Errorf("HTTP status %s - %s: %s", res.Status,
+				oneDriveError.Error.Code, oneDriveError.Error.Message)
+		case http.StatusUnauthorized: // Unauthorized
+			return nil, fmt.Errorf("%w: HTTP status %s - %s: %s", ErrReauthRequired, res.Status, oneDriveError.Error.Code, oneDriveError.Error.Message)
+		case http.StatusForbidden: // Forbidden
+			// Specific handling for Forbidden
+		case http.StatusNotFound, http.StatusGone: // Not Found
+			return nil, fmt.Errorf("HTTP status %s - %s: %s", res.Status, oneDriveError.Error.Code, oneDriveError.Error.Message)
+		case http.StatusConflict: // Conflict
+			// Specific handling for Conflict
+		case http.StatusLengthRequired: // Length Required
+			// Specific handling for Length Required
+		case http.StatusPreconditionFailed: // Precondition Failed
+			// Specific handling for Precondition Failed
+		case http.StatusRequestEntityTooLarge: // Request Entity Too Large
+			// Specific handling for Request Entity Too Large
+		case http.StatusUnsupportedMediaType: // Unsupported Media Type
+			// Specific handling for Unsupported Media Type
+		case http.StatusRequestedRangeNotSatisfiable: // Requested Range Not Satisfiable
+			// Specific handling for Requested Range Not Satisfiable
+		case http.StatusUnprocessableEntity: // Unprocessable Entity
+			// Specific handling for Unprocessable Entity
+		case http.StatusTooManyRequests, http.StatusInternalServerError,
+			http.StatusServiceUnavailable, 509: // Too Many Requests
+			return nil, fmt.Errorf("%w: %v", ErrRetryLater, err)
+		case http.StatusNotImplemented: // Not Implemented
+			// Specific handling for Not Implemented
+		case http.StatusInsufficientStorage: // Insufficient Storage
+			// Specific handling for Insufficient Storage
 		default:
-			return nil, fmt.Errorf("HTTP error: %s: %s", res.Status, string(resBody))
+			return nil, fmt.Errorf("HTTP error: %s - %s", res.Status, oneDriveError.Error.Code)
+		}
+
+		// Handling based on OneDrive-specific error codes
+		switch oneDriveError.Error.Code {
+		case "accessDenied":
+			// Handle access denied
+		case "activityLimitReached":
+			// Handle activity limit reached
+		case "generalException":
+			// Handle general exception
+		case "invalidRange":
+			// Handle invalid range
+		case "invalidRequest":
+			// Handle invalid request
+		case "itemNotFound":
+			// Handle item not found
+		case "malwareDetected":
+			// Handle malware detected
+		case "nameAlreadyExists":
+			// Handle name already exists
+		case "notAllowed":
+			// Handle not allowed
+		case "notSupported":
+			// Handle not supported
+		case "resourceModified":
+			// Handle resource modified
+		case "resyncRequired":
+			// Handle resync required
+		case "serviceNotAvailable":
+			// Handle service not available
+		case "quotaLimitReached":
+			// Handle quota limit reached
+		case "unauthenticated":
+			// Handle unauthenticated
+		default:
+			return nil, fmt.Errorf("OneDrive error: %s - %s", res.Status, oneDriveError.Error.Code)
 		}
 	}
 
@@ -74,7 +152,7 @@ func apiCall(client *http.Client, url string) (*http.Response, error) {
 
 // GetMyDrives retrieves the drives information.
 func GetMyDrives(client *http.Client) error {
-	res, err := apiCall(client, rootUrl+"me/drive/root/children")
+	res, err := apiCall(client, "GET", rootUrl+"me/drive/root/children")
 	if err != nil {
 		return err
 	}
